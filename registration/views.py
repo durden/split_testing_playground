714f55a93e06a4603c209aa27fd895e0317bfe9b
Luke Lee <durdenmisc@gmail.com>
2012-05-27 12:32:46 -0500

    Process form

--- registration/views.py
+++ registration/views.py
@@ -6,4 +6,7 @@ from django.shortcuts import render
 
 
 def home(request, template_name="registration/register.html"):
+    if request.method == 'POST':
+        template_name = 'registration/thanks.html'
+
     return render(request, template_name)
