3e97431ce9536d4b6a4e81c97a74244c1075d751
Luke Lee <durdenmisc@gmail.com>
2012-05-27 13:25:35 -0500

    Basic code to switch between ab test variations

--- registration/views.py
+++ registration/views.py
@@ -3,10 +3,46 @@ Views for registration app
 """
 
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.utils import timezone
+
+from registration.models import ABTest, TestChoice, TestResult
+
+
+def _get_choice_for_split_test(view_method):
+    """
+    Get a choice_id for a split test associated with the given view method
+
+    view_method can be direct reference to view method or string of method
+    Example: 'registration.views.home'
+
+    If no split test is running for the given url, 0 will be returned.
+    """
+
+    # choice_id 0 will show the 'control' in template so if no split test is
+    # running users will automatically see the regular template
+    choice = 0
+
+    tests = ABTest.objects.filter(url=reverse(view_method))
+    for test in tests:
+        now = timezone.now()
+        if test.start_date <= now and test.end_date > now:
+            choices = TestChoice.objects.filter(test=test)
+            results = TestResult.objects.filter(choice__in=list(choices))
+            num_results = sum([result.visitors for result in results])
+
+            # Evenly distribute test across all choices by modding by
+            # number of results for the entire test
+            choice = choices[num_results % len(choices)].id
+
+    return choice
 
 
 def home(request, template_name="registration/register.html"):
     if request.method == 'POST':
         template_name = 'registration/thanks.html'
+    else:
+        # See if there's a test running for this url
+        choice = _get_choice_for_split_test('registration.views.home')
 
-    return render(request, template_name)
+    return render(request, template_name, {'test_choice': choice})
