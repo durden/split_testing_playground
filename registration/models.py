d3a952bad29f4abba1296907471ab58d4516bf48
Luke Lee <durdenmisc@gmail.com>
2012-05-27 13:11:24 -0500

    Add admin friendly names, url field

--- registration/models.py
+++ registration/models.py
@@ -5,8 +5,14 @@ class ABTest(models.Model):
     """Model to hold information about AB split test"""
 
     name = models.CharField(max_length=300)
+    url = models.CharField(max_length=200)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
+
+    def __unicode__(self):
+        """return unicode representation of object"""
+
+        return u'%s' % (self.name)
 
 
 class TestChoice(models.Model):
@@ -18,11 +24,21 @@ class TestChoice(models.Model):
     test = models.ForeignKey(ABTest)
     description = models.TextField()
 
+    def __unicode__(self):
+        """return unicode representation of object"""
+
+        return u'%s' % (self.description)
+
 
 class TestResult(models.Model):
     """Model to hold conversion results of a particular test choice"""
 
-
     visitors = models.BigIntegerField()
     conversions = models.BigIntegerField()
     choice = models.ForeignKey(TestChoice)
+
+    def __unicode__(self):
+        """return unicode representation of object"""
+
+        return u'Choice: %s, Visitors: %d, Conversions: %d' % (
+                    self.choice.description, self.visitors, self.conversions)
