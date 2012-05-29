341cb418eb4625e30980d81602ecae3ebd217ebb
Luke Lee <durdenmisc@gmail.com>
2012-05-29 14:28:38 -0500

    Add hidden field choice to django form

--- registration/forms.py
+++ registration/forms.py
@@ -6,3 +6,4 @@ class RegistrationForm(forms.Form):
     email = forms.EmailField()
     first_name = forms.CharField(required=False)
     last_name = forms.CharField(required=False)
+    choice = forms.IntegerField(widget=forms.HiddenInput, required=False)
